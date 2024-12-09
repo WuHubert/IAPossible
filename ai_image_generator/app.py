3442bb72f14f15515e119d5ea957ee04961cfa02
1110410010 <95218245+WuHubert@users.noreply.github.com>
2024-12-09 14:00:32 +0800

    TX V5.3

--- ai_image_generator/app.py
+++ ai_image_generator/app.py
@@ -5,3 +5,8 @@ from routes.image_routes import image_bp
 app = Flask(__name__)
 app.register_blueprint(image_bp)
 
+if __name__ == '__main__':
+    app.run(debug=True)
+else:
+    # 确保 Gunicorn 可以访问到 app 变量
+    application = app
