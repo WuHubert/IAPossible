997736d0dc42be1a2eafee761fb26e8229a3d462
1110410010 <95218245+WuHubert@users.noreply.github.com>
2024-12-09 11:37:02 +0800

    T1 V2

--- ai_image_generator/app.py
+++ ai_image_generator/app.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from routes.config import API_CONFIG
 from routes.image_routes import image_bp
+from asgiref.wsgi import WsgiToAsgi
 
 app = Flask(__name__)
-app.register_blueprint(image_bp) 
+app.register_blueprint(image_bp, url_prefix='/image')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+asgi_app = WsgiToAsgi(app)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(asgi_app, host="0.0.0.0", port=8000)
