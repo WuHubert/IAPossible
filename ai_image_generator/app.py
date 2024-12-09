4586c6ec9b6d0dbbaa272a828625f553ee059d34
1110410010 <95218245+WuHubert@users.noreply.github.com>
2024-12-09 13:01:41 +0800

    TX V5

--- ai_image_generator/app.py
+++ ai_image_generator/app.py
@@ -3,7 +3,7 @@ from routes.config import API_CONFIG
 from routes.image_routes import image_bp
 
 app = Flask(__name__)
-app.register_blueprint(image_bp, url_prefix='/image')
+app.register_blueprint(image_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
